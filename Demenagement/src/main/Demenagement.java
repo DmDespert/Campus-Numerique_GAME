631548606d3afea6b727491b0c98a27280a9777d
DmDespert <dimitri.despert@gmail.com>
2021-04-27 10:00:58 +0200

    feat: demenagement working

---
+++ Demenagement/src/main/Demenagement.java
@@ -0,0 +1,17 @@
+public class Demenagement {
+	public static void main (String [] args) {
+		 int i = 34;
+		 
+		 while (i > 0) {
+			 if (i > 8) {
+				 System.out.print("Un voyage de 9 cartons a été effectué \n");
+				 
+				 i = i - 9; 
+			 }
+			 else {
+				 System.out.print("Un voyage de " + i + " cartons a été effectué \n");
+				 i = i - i;
+			 }
+		 }
+	}
+}
